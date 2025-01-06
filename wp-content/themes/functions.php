d98aacdf46ae0173e63325ac8da7245fd129e112
vivvoweb <hestv.tv@gmail.com>
2025-01-06 19:45:14 +0100

    Enregistrement des scripts JS dans WordPress

--- wp-content/themes/functions.php
+++ wp-content/themes/functions.php
@@ -11,3 +11,12 @@ function Nathalie_footer_menu() {
     register_nav_menu('footer-menu', __('Menu du pied de page', 'text-domain')); // Enregistre le menu du pied de page
 }
 add_action('after_setup_theme', 'Nathalie_footer_menu');
+
+
+// Enregistrer les scripts JS
+function Nathalie_enqueue_scripts() {
+    
+    wp_enqueue_script('Nathalie_modal_contact.js', get_template_directory_uri() . '/js/Nathalie_modal_contact.js', array('jquery'), '1.0', true); // Modale de contact
+    
+}
+add_action('wp_enqueue_scripts', 'Nathalie_enqueue_scripts'); // Ajoute les scripts aux pages du site
