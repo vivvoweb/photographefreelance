e27ded96276ccaaf1022e023ff23c8ac3e7782db
vivvoweb <hestv.tv@gmail.com>
2025-01-06 19:33:38 +0100

    Ajout de la modale de contact dans le footer du site

--- wp-content/themes/footer.php
+++ wp-content/themes/footer.php
@@ -1,15 +1,16 @@
 <footer>
     <nav class="footer-menu">
         <?php
-        // Implémentation d'un footer avec un menu de pied de page pour le site WordPress
+        // Affiche le menu de pied de page WordPress
         wp_nav_menu([
             'theme_location' => 'footer-menu', // Spécifie l'emplacement du menu dans le thème
         ]);
         ?>
     </nav>
 </footer>
-
 <?php wp_footer(); ?>
+<?php get_template_part('template_parts/modal/Nathalie_contact-modal-photo.php'); ?>
+
 </body>
 </html>
 
